ed240c0f983fbeb5eee3a183905c77aa4b9f1bb7
felipeh3005 <felipeh3005@gmail.com>
2025-10-24 14:32:31 -0500

    configurtacion de ESLint con Airbnb y correcion de archivos segun ESLint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    console.log('¡Bienvenido a Brain Games!');
-    const name = readlineSync.question('¿Cuál es tu nombre? ');
-    console.log(`¡Hola, ${name}!`);
+const askName = () => {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('¿Cuál es tu nombre? ');
+  console.log(`¡Hola, ${name}!`);
 };
+
+export default askName;
